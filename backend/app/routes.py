efee4f9799a5460961a4b7d2bdada9b7804d2a78
gdmello <gavin.dmello@points.com>
2016-07-28 14:29:22 -0400

    initial mock of endpoint POST /challenges/<challenge_id>/user/<user_id>

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -14,27 +14,42 @@ def fitbit_user():
     fitbit_token = request.args.get('fitbit_token', '')
     user = {
         'fitbitToken': fitbit_token,
-        'identifier': 123
+        'userId': 123
     }
     return Response(json.dumps(user), status=httplib.CREATED, mimetype='application/json')
 
 
-@app.route('/user/<userid>/challenges/_<status>', methods=['GET'])
-def challenge_status(userid, status):
+@app.route('/user/<user_id>/challenges/_<status>', methods=['GET'])
+def challenge_status(user_id, status):
     """
-    GET /user/<userid>/challenges/_new
-    GET /user/<userid>/challenges/_accepted
-    :param userid:
+    GET /user/<user_id>/challenges/_new
+    GET /user/<user_id>/challenges/_accepted
+    :param user_id:
     :param status:
     :return:
     """
     challenges = [{
         'challengeName': 'some name',
-        'identifier': 123,
-        'userIdentifier': userid,
+        'challengeId': 123,
+        'userId': user_id,
         'status': status
     }]
     return Response(json.dumps(challenges), status=httplib.OK, mimetype='application/json')
+
+
+@app.route('challenges/<challenge_id>/user/<user_id>', methods=['POST'])
+def user_challenge(challenge_id, user_id):
+    """
+        POST /challenges/<challenge_id>/user/<user_id>
+    :param challenge_id:
+    :param user_id:
+    :return:
+    """
+    user = {
+        'challengeId': challenge_id,
+        'userId': user_id
+    }
+    return Response(json.dumps(user), status=httplib.CREATED, mimetype='application/json')
 
 
 if __name__ == '__main__':
