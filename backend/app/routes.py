76e7a3fc130f0b57ef7cfe3f4a4927a6e82a5c4c
gdmello <gavin.dmello@points.com>
2016-07-28 15:54:50 -0400

    Allow cross site scripting ... yay this is only for a hackathon demo!

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -5,7 +5,7 @@ import logging
 import sys
 
 from flask import Flask, request, Response
-
+from flask_cors import CORS
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -13,6 +13,7 @@ handler = logging.StreamHandler(sys.stdout)
 logger.addHandler(handler)
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/user', methods=['POST'])
@@ -89,16 +90,16 @@ def initialize_db():
     connection = sqlite3.connect('pointsboost.db')
     cursor = connection.cursor()
     cursor.execute(''' CREATE TABLE IF NOT EXISTS users
-                        (identifier integer PRIMARY KEY ASC, email text, name text, loyalty_program_user_id text,
-                        fitbit_access_token text, fitbit_refresh_token text, fitbit_token_expiry text,
-                        fitbit_id text)''')
+                        (identifier INTEGER PRIMARY KEY ASC, email TEXT, name TEXT, loyalty_program_user_id TEXT,
+                        fitbit_access_token TEXT, fitbit_refresh_token TEXT, fitbit_token_expiry TEXT,
+                        fitbit_id TEXT)''')
     cursor.execute(''' CREATE TABLE IF NOT EXISTS challenge
-                        (identifier integer PRIMARY KEY ASC, name text, steps_to_unlock integer, loyalty_program_merchant_user_id text,
-                        expiry_timestamp text, reward_points integer)''')
+                        (identifier INTEGER PRIMARY KEY ASC, name TEXT, steps_to_unlock INTEGER, loyalty_program_merchant_user_id TEXT,
+                        expiry_timestamp TEXT, reward_points INTEGER)''')
     cursor.execute(''' CREATE TABLE IF NOT EXISTS user_challenge
-                        (user_identifier integer, challenge_identifier integer,
-                        user_total_step_count_on_start integer, user_total_step_count_on_expiry integer,
-                        status text, PRIMARY KEY (user_identifier, challenge_identifier),
+                        (user_identifier INTEGER, challenge_identifier INTEGER,
+                        user_total_step_count_on_start INTEGER, user_total_step_count_on_expiry INTEGER,
+                        status TEXT, PRIMARY KEY (user_identifier, challenge_identifier),
                         FOREIGN KEY(user_identifier) REFERENCES users(identifier),
                         FOREIGN KEY(challenge_identifier) REFERENCES challenge(identifier))''')
 
