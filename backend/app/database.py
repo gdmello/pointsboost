083a6c53d70a043bc252d6adf6aac838eda53841
gdmello <gavin.dmello@points.com>
2016-07-28 18:00:52 -0400

    seed challenges.

--- backend/app/database.py
+++ backend/app/database.py
@@ -10,7 +10,7 @@ def initialize():
                         (identifier TEXT PRIMARY KEY ASC, email TEXT, name TEXT, loyalty_program_user_id TEXT,
                         fitbit_access_token TEXT, fitbit_refresh_token TEXT, fitbit_token_expiry TEXT,
                         fitbit_id TEXT)''')
-    cursor.execute(''' CREATE TABLE IF NOT EXISTS challenge
+    cursor.execute(''' CREATE TABLE IF NOT EXISTS challenges
                         (identifier TEXT PRIMARY KEY ASC, name TEXT, steps_to_unlock INTEGER, loyalty_program_merchant_user_id TEXT,
                         expiry_timestamp TEXT, reward_points INTEGER)''')
     cursor.execute(''' CREATE TABLE IF NOT EXISTS user_challenge
@@ -18,7 +18,19 @@ def initialize():
                         user_total_step_count_on_start INTEGER, user_total_step_count_on_expiry INTEGER,
                         status TEXT, PRIMARY KEY (user_identifier, challenge_identifier),
                         FOREIGN KEY(user_identifier) REFERENCES users(identifier),
-                        FOREIGN KEY(challenge_identifier) REFERENCES challenge(identifier))''')
+                        FOREIGN KEY(challenge_identifier) REFERENCES challenges(identifier))''')
+    _seed_challenges(cursor)
+
+
+def _seed_challenges(cursor):
+    challenges = [
+        ('1', 'Get 100 GR Points For 10 steps', 10, 'merchant_GR_123', '2016-10-01 12:12:12.777', 100),
+        ('2', 'Get 200 GR Points For 20 steps', 20, 'merchant_GR_123', '2016-10-01 12:12:12.777', 200),
+        ('3', 'Get 300 GR Points For 30 steps', 30, 'merchant_GR_123', '2016-10-01 12:12:12.777', 300),
+        ('4', 'Get 400 GR Points For 40 steps', 40, 'merchant_GR_123', '2016-10-01 12:12:12.777', 400),
+        ('5', 'Get 500 GR Points For 50 steps', 50, 'merchant_GR_123', '2016-10-01 12:12:12.777', 500)
+    ]
+    cursor.execute('INSERT INTO challenges VALUES(?,?,?,?,?,?)', challenges)
 
 
 def create_user(name, email, loyalty_program_user_id, access_token, refresh_token, token_expiry, fitbit_id):
